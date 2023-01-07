8672187203b619ea947d4f1df08b7d0dea822214
Eunji Kang <etothej@naver.com>
2023-01-07 23:48:33 +0900

    ADD: 페이지 폴더 수정 - 메인페이지 form 추가 - 미리보기 페이지 타이틀 컴포넌트 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionContainer from 'components/QuestionContainer';
+import TitleContainer from 'components/TitleContainer';
+import { useSelector } from 'react-redux';
 
 const Main = () => {
-  return <div>MAIN</div>;
+  const formInfo = useSelector(state => state.form);
+  const questions = useSelector(state => state.question);
+  const [info, setInfo] = useState({
+    title: formInfo.title,
+    description: '',
+  });
+
+  const onChangeInfo = e => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+    // console.log('hello');
+  };
+
+  return (
+    <div>
+      <TitleContainer info={info} onChangeInfo={onChangeInfo} />
+      {questions.map(question => (
+        <QuestionContainer
+          key={question.id}
+          question={question}
+          questionId={question.id}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/View/index.jsx
+++ src/pages/View/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import TitleContainer from 'components/TitleContainer';
+import { useSelector } from 'react-redux';
 
 const View = () => {
-  return <div />;
+  const formInfo = useSelector(state => state.form);
+
+  return (
+    <div>
+      Preview
+      <TitleContainer info={formInfo} />
+    </div>
+  );
 };
 
 export default View;
